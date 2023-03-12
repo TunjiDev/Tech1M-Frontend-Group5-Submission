cd0dd8c03997b18d742a958caf055bcbf0cd9ef0
Adetunji Igbatayo <adetunjiigbatayo@gmail.com>
2023-03-12 18:10:59 +0100

    feat: build login screen

---
+++ src/components/LoginPageContent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import styles from "./LoginPageContent.module.css";
+
+function LoginPageContent() {
+  return (
+    <Box className={styles.background} h={"100vh"}>
+      LOGIN PAGE IN PROGRESS....
+    </Box>
+  );
+}
+
+export default LoginPageContent;
