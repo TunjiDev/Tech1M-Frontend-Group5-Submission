9be0ccb4dbf0d4acb8ef82e901e18b42dce876ea
Idowu Deborah <idowudeborah001@gmail.com>
2023-03-14 01:16:46 +0100

    name

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import HomePageContent from "../components/HomePageContent";
+import { Helmet } from "react-helmet-async";
 
 function HomePage() {
-  return <HomePageContent />;
+  return (
+    <>
+      <Helmet>
+        <title>Welcome</title>
+        <meta
+          name="description"
+          content="Get ready to explore the exciting world of
+          technology and learn new skills that will help you shape your future."
+        />
+        <link rel="canonical" href="/" />
+      </Helmet>
+      <HomePageContent />
+    </>
+  );
 }
 
 export default HomePage;
