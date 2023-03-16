da876794d8362979a248ffe974bcd8bfd1bc363c
Adetunji Igbatayo <adetunjiigbatayo@gmail.com>
2023-03-16 12:39:46 +0100

    feat: work on dashboard

--- src/components/InstructorDashboardPageContent.jsx
+++ src/components/InstructorDashboardPageContent.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { Box, Flex, Text, Heading, Avatar, Progress, AvatarGroup } from "@chakra-ui/react";
+import { Box, Flex, Text, Heading, Avatar, Progress, Badge } from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
+import { CiBellOn } from "react-icons/ci";
 import student1 from "../assets/images/student-progress/student1.png";
 import student2 from "../assets/images/student-progress/student2.png";
 import student3 from "../assets/images/student-progress/student3.png";
 import student4 from "../assets/images/student-progress/student4.png";
 import student5 from "../assets/images/student-progress/student5.png";
 import student6 from "../assets/images/student-progress/student6.png";
-import lesson1 from "../assets/images/upcoming-lessons/lesson1.png";
-import lesson2 from "../assets/images/upcoming-lessons/lesson2.png";
-import lesson3 from "../assets/images/upcoming-lessons/lesson3.png";
-import lesson4 from "../assets/images/upcoming-lessons/lesson4.png";
-import lesson5 from "../assets/images/upcoming-lessons/lesson5.png";
 
 function InstructorDashboardPageContent() {
   return (
@@ -30,7 +26,7 @@ function InstructorDashboardPageContent() {
             Welcome back
           </Text>
           <Heading as={"h3"} size="lg" color={"#E6F0FF"}>
-            Adams Chuks
+            Will Buyers
           </Heading>
           <Text fontSize={"0.75rem"} color={"#FFFEFE"}>
             Your students completed 74% of the tasks.
@@ -48,7 +44,9 @@ function InstructorDashboardPageContent() {
               </Box>
               <Box flex={4}>
                 <Flex justifyContent={"space-between"} mb={".5rem"}>
-                  <Text color={"#6BA6FF"}>Smith Blacks</Text>
+                  <Text color={"#6BA6FF"} fontSize={".75rem"}>
+                    Smith Blacks
+                  </Text>
                   <Text color={"#6BA6FF"}>36%</Text>
                 </Flex>
                 <Box>
@@ -63,7 +61,9 @@ function InstructorDashboardPageContent() {
               </Box>
               <Box flex={4}>
                 <Flex justifyContent={"space-between"} mb={".5rem"}>
-                  <Text color={"#6BA6FF"}>Adeola Bats</Text>
+                  <Text color={"#6BA6FF"} fontSize={".75rem"}>
+                    Adeola Bats
+                  </Text>
                   <Text color={"#6BA6FF"}>65%</Text>
                 </Flex>
                 <Box>
@@ -78,7 +78,9 @@ function InstructorDashboardPageContent() {
               </Box>
               <Box flex={4}>
                 <Flex justifyContent={"space-between"} mb={".5rem"}>
-                  <Text color={"#6BA6FF"}>Ying Toms</Text>
+                  <Text color={"#6BA6FF"} fontSize={".75rem"}>
+                    Ying Toms
+                  </Text>
                   <Text color={"#6BA6FF"}>24%</Text>
                 </Flex>
                 <Box>
@@ -93,7 +95,9 @@ function InstructorDashboardPageContent() {
               </Box>
               <Box flex={4}>
                 <Flex justifyContent={"space-between"} mb={".5rem"}>
-                  <Text color={"#6BA6FF"}>Tailor Hing</Text>
+                  <Text color={"#6BA6FF"} fontSize={".75rem"}>
+                    Tailor Hing
+                  </Text>
                   <Text color={"#6BA6FF"}>72%</Text>
                 </Flex>
                 <Box>
@@ -108,7 +112,9 @@ function InstructorDashboardPageContent() {
               </Box>
               <Box flex={4}>
                 <Flex justifyContent={"space-between"} mb={".5rem"}>
-                  <Text color={"#6BA6FF"}>Smith Blacks</Text>
+                  <Text color={"#6BA6FF"} fontSize={".75rem"}>
+                    Smith Blacks
+                  </Text>
                   <Text color={"#6BA6FF"}>45%</Text>
                 </Flex>
                 <Box>
@@ -123,7 +129,9 @@ function InstructorDashboardPageContent() {
               </Box>
               <Box flex={4}>
                 <Flex justifyContent={"space-between"} mb={".5rem"}>
-                  <Text color={"#6BA6FF"}>Smith Blacks</Text>
+                  <Text color={"#6BA6FF"} fontSize={".75rem"}>
+                    Smith Blacks
+                  </Text>
                   <Text color={"#6BA6FF"}>16%</Text>
                 </Flex>
                 <Box>
@@ -150,37 +158,6 @@ function InstructorDashboardPageContent() {
           </Box>
 
           <Box flex={1.1} ml={"1rem"} pt={".5rem"}>
-            <Flex
-              justifyContent={"space-around"}
-              boxShadow={"0px 1px 2px rgba(0, 0, 0, 0.1)"}
-              borderRadius={".5rem"}
-              py={"1rem"}
-              mb={".5rem"}
-            >
-              <Box>
-                <Text textAlign={"start"} color={"#01A830"}>
-                  Total Courses
-                </Text>
-                <Text textAlign={"start"} color={"#003E9C"} fontWeight={500}>
-                  215
-                </Text>
-              </Box>
-              <Flex
-                justifyContent={"center"}
-                alignItems={"center"}
-                border={"1px solid #0065FF"}
-                borderRadius={".5rem"}
-                py={".3rem"}
-                w={"40%"}
-                cursor={"pointer"}
-              >
-                <Box mr={".5rem"}>
-                  <BsThreeDots color={"#0065FF"} />
-                </Box>
-                <Text color={"#0065FF"}>View All</Text>
-              </Flex>
-            </Flex>
-
             <Flex
               justifyContent={"space-around"}
               boxShadow={"0px 1px 2px rgba(0, 0, 0, 0.1)"}
@@ -243,115 +220,128 @@ function InstructorDashboardPageContent() {
               </Flex>
             </Flex>
 
-            <Box bg={"#F5F6F7"} py={"1rem"} px={"1rem"} borderRadius={".5rem"}>
+            <Box py={"1rem"} px={"1rem"} borderRadius={".5rem"} boxShadow={"0px 1px 2px rgba(0, 0, 0, 0.1)"}>
               <Text textAlign={"start"} color={"#003E9C"} mb={"1rem"}>
-                Work Stats
+                Messages
               </Text>
 
-              <Flex justifyContent={"space-between"}>
-                <Box mr={"1.5rem"}>
-                  <Flex flexWrap={"wrap"} justifyContent={"flex-start"} textAlign={"start"} color={"#7A8699"}>
-                    Lesson Discuss
-                  </Flex>
-                  <Text textAlign={"start"} color={"#96C0FF"} fontSize={".75rem"} fontWeight={500}>
-                    3.4hrs
-                  </Text>
+              <Box mb={"1rem"}>
+                <Flex justifyContent={"flex-start"} alignItems={"center"}>
+                  <Box mr={".5rem"}>
+                    <Avatar name="Smith Blacks" src={student1} />
+                  </Box>
+                  <Box>
+                    <Text color={"#6BA6FF"} mb={".5rem"} fontSize={".75rem"}>
+                      Smith Blacks
+                    </Text>
+                    <Box mr={"1rem"} position="relative">
+                      <CiBellOn color={"#96C0FF"} size={25} />
+                      <Badge
+                        position={"absolute"}
+                        top={-1.5}
+                        right={12}
+                        bg={"#FA0724"}
+                        color={"#FFFEFE"}
+                        fontWeight={500}
+                        fontSize={"xs"}
+                        borderRadius={"50%"}
+                      >
+                        3
+                      </Badge>
+                    </Box>
+                  </Box>
+                </Flex>
+              </Box>
+
+              <Flex
+                justifyContent={"center"}
+                alignItems={"center"}
+                border={"1px solid #0065FF"}
+                borderRadius={".5rem"}
+                py={".5rem"}
+                cursor={"pointer"}
+              >
+                <Box mr={".5rem"}>
+                  <BsThreeDots color={"#0065FF"} />
                 </Box>
-                <Box mr={"1.5rem"}>
-                  <Flex flexWrap={"wrap"} justifyContent={"flex-start"} textAlign={"start"} color={"#7A8699"}>
-                    Taking Class
-                  </Flex>
-                  <Text textAlign={"start"} color={"#96C0FF"} fontSize={".75rem"} fontWeight={500}>
-                    4.5hrs
-                  </Text>
-                </Box>
-                <Box>
-                  <Flex flexWrap={"wrap"} justifyContent={"flex-start"} textAlign={"start"} color={"#7A8699"}>
-                    Work in Progress
-                  </Flex>
-                  <Text textAlign={"start"} color={"#96C0FF"} fontSize={".75rem"} fontWeight={500}>
-                    918 +23.45%
-                  </Text>
-                </Box>
+                <Text color={"#0065FF"}>View All</Text>
               </Flex>
             </Box>
           </Box>
 
-          <Box flex={0.8} ml={"1rem"} mr={"3rem"}>
-            <Text textAlign={"start"} color={"#003E9C"}>
-              Upcoming Lessons
-            </Text>
-            <Box bg={"#F5F6F7"} py={".5rem"} pl={"1rem"} borderRadius={".5rem"} mb={".5rem"}>
+          <Box
+            flex={0.8}
+            ml={"1rem"}
+            mr={"3rem"}
+            boxShadow={"0px 3px 4px rgba(0, 0, 0, 0.15)"}
+            px={".5rem"}
+            py={".5rem"}
+            borderRadius={".5rem"}
+          >
+            <Box py={".5rem"}>
+              <Text textAlign={"start"} color={"#0065FF"} mb={"1rem"}>
+                Assignments
+              </Text>
               <Text textAlign={"start"} color={"#2B7FFF"}>
-                UI/UX Design Thinking
+                Redesign your favorite Application
               </Text>
-              <Text color={"#2B7FFF"} textAlign={"start"} fontSize={".75rem"} fontWeight={500}>
-                12:30am - 1:15pm
+              <Text color={"#0047B3"} textAlign={"start"} fontSize={".75rem"} fontWeight={500}>
+                submissions
               </Text>
-              <Box>
-                <AvatarGroup size="sm" spacing={-5}>
-                  <Avatar name="lesson 5" src={lesson5} />
-                  <Avatar name="lesson 4" src={lesson4} />
-                  <Avatar name="lesson 3" src={lesson3} />
-                  <Avatar name="lesson 2" src={lesson2} />
-                  <Avatar name="lesson 1" src={lesson1} />
-                </AvatarGroup>
-              </Box>
+              <Text textAlign={"start"} color={"#0047B3"} fontSize={"1.25rem"}>
+                200
+              </Text>
             </Box>
 
-            <Box bg={"#F5F6F7"} py={".5rem"} pl={"1rem"} borderRadius={".5rem"} mb={".5rem"}>
+            <Box py={".5rem"}>
               <Text textAlign={"start"} color={"#2B7FFF"}>
-                UI Motion Fundamentals
+                Wireframe your favorite Application
               </Text>
-              <Text color={"#2B7FFF"} textAlign={"start"} fontSize={".75rem"} fontWeight={500}>
-                1:35pm - 2:15pm
+              <Text color={"#0047B3"} textAlign={"start"} fontSize={".75rem"} fontWeight={500}>
+                submissions
               </Text>
-              <Box>
-                <AvatarGroup size="sm" spacing={-5}>
-                  <Avatar name="lesson 5" src={lesson5} />
-                  <Avatar name="lesson 4" src={lesson4} />
-                  <Avatar name="lesson 3" src={lesson3} />
-                  <Avatar name="lesson 2" src={lesson2} />
-                  <Avatar name="lesson 1" src={lesson1} />
-                </AvatarGroup>
-              </Box>
+              <Text textAlign={"start"} color={"#0047B3"} fontSize={"1.25rem"}>
+                300
+              </Text>
             </Box>
 
-            <Box bg={"#F5F6F7"} py={".5rem"} pl={"1rem"} borderRadius={".5rem"} mb={".5rem"}>
+            <Box py={".5rem"}>
               <Text textAlign={"start"} color={"#2B7FFF"}>
-                Web Development Fundamentals
+                Do a research on design thinking
               </Text>
-              <Text color={"#2B7FFF"} textAlign={"start"} fontSize={".75rem"} fontWeight={500}>
-                2:30pm - 2:45pm
+              <Text color={"#0047B3"} textAlign={"start"} fontSize={".75rem"} fontWeight={500}>
+                submissions
               </Text>
-              <Box>
-                <AvatarGroup size="sm" spacing={-5}>
-                  <Avatar name="lesson 5" src={lesson5} />
-                  <Avatar name="lesson 4" src={lesson4} />
-                  <Avatar name="lesson 3" src={lesson3} />
-                  <Avatar name="lesson 2" src={lesson2} />
-                  <Avatar name="lesson 1" src={lesson1} />
-                </AvatarGroup>
-              </Box>
+              <Text textAlign={"start"} color={"#0047B3"} fontSize={"1.25rem"}>
+                1700
+              </Text>
             </Box>
 
-            <Box bg={"#F5F6F7"} py={".5rem"} pl={"1rem"} borderRadius={".5rem"} mb={".5rem"}>
+            <Box py={".5rem"} mb={".5rem"}>
               <Text textAlign={"start"} color={"#2B7FFF"}>
-                User Research Methodologies
+                Read up on ideation
               </Text>
-              <Text color={"#2B7FFF"} textAlign={"start"} fontSize={".75rem"} fontWeight={500}>
-                4:30pm - 4:15pm
+              <Text color={"#0047B3"} textAlign={"start"} fontSize={".75rem"} fontWeight={500}>
+                submissions
               </Text>
-              <Box>
-                <AvatarGroup size="sm" spacing={-5}>
-                  <Avatar name="lesson 5" src={lesson5} />
-                  <Avatar name="lesson 4" src={lesson4} />
-                  <Avatar name="lesson 3" src={lesson3} />
-                  <Avatar name="lesson 2" src={lesson2} />
-                  <Avatar name="lesson 1" src={lesson1} />
-                </AvatarGroup>
-              </Box>
+              <Text textAlign={"start"} color={"#0047B3"} fontSize={"1.25rem"}>
+                1750
+              </Text>
             </Box>
+
+            <Flex
+              justifyContent={"center"}
+              alignItems={"center"}
+              border={"1px solid #0065FF"}
+              borderRadius={".5rem"}
+              py={".5rem"}
+              cursor={"pointer"}
+            >
+              <Box mr={".5rem"}>
+                <BsThreeDots color={"#0065FF"} />
+              </Box>
+              <Text color={"#0065FF"}>View All</Text>
+            </Flex>
           </Box>
         </Flex>
       </Box>
