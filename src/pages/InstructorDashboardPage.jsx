ed903a6adf33ed63abbeb8c4fa367a102f0d6e46
Idowu Deborah <idowudeborah001@gmail.com>
2023-03-16 14:04:13 +0100

    I added Seo to the Admin Dashboard Page and deleted the Dashboard page.

--- src/pages/InstructorDashboardPage.jsx
+++ src/pages/InstructorDashboardPage.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import InstructorDashboardPageContent from "../components/InstructorDashboardPageContent";
+import { Helmet } from "react-helmet-async";
 
 function InstructorDashboardPage() {
-  return <InstructorDashboardPageContent />;
+  return (
+    <>
+      <Helmet>
+        <title>Instructor Dashboard</title>
+        <meta name="description" content="Welcome to your Dashboard!" />
+        <link rel="canonical" href="./pages/InstructorDashboardPage" />
+      </Helmet>
+      <InstructorDashboardPageContent />
+    </>
+  );
 }
 
 export default InstructorDashboardPage;
