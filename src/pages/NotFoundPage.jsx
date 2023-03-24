5912331753adaa55a06268d4708fe400de6568cf
Adetunji Igbatayo <adetunjiigbatayo@gmail.com>
2023-03-24 16:35:42 +0100

    finish seo

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import NotFoundPageContent from "../components/NotFoundPageContent";
+import { Helmet } from "react-helmet-async";
 
 function NotFound() {
-  return <NotFoundPageContent />;
+  return (
+    <>
+      <Helmet>
+        <meta name="robots" content="noindex" />
+      </Helmet>
+      <NotFoundPageContent />;
+    </>
+  );
 }
 
 export default NotFound;
